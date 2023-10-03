4495e74d05887cde72b132a515b5e1fa0d84718e
Nicholas Miller <nicholas.anthony.miller@gmail.com>
2023-10-03 00:54:09 -0400

    working up to 3.5.1

---
+++ tests/health_check.rs
@@ -0,0 +1,21 @@
+use actix_web::dev::Server;
+
+#[tokio::test]
+async fn health_check_works(){
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind server");
+    let _ = tokio::spawn(server);
+}
